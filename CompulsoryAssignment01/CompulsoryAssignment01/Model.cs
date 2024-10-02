9913cdcf5a57c2dedcab03173c2f1d9321dce6fd
Bjørn Göttler <bjoern@goettler.dk>
2024-10-02 19:49:31 +0200

    :card_file_box: Add Ratings model

--- CompulsoryAssignment01/CompulsoryAssignment01/Model.cs
+++ CompulsoryAssignment01/CompulsoryAssignment01/Model.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
 namespace CompulsoryAssignment01;
@@ -14,10 +15,19 @@ public class Product
     public string Name { get; set; }
     public int CategoryId { get; set; }
     public Category Category { get; set; }
+    public ICollection<Rating> Rating { get; set; }
 }
 
 public class Category
 {
     public int Id { get; set; }
     public string Name { get; set; }
+}
+
+public class Rating
+{
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    [Column(TypeName = "decimal(18,4)")]
+    public decimal Score { get; set; }
 }
