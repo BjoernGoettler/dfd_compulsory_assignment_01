ce1dae3f6a32e27e9dde1601bff673155351a565
Bjørn Göttler <bjoern@goettler.dk>
2024-10-02 19:44:28 +0200

    :card_file_box: Add categories model

--- CompulsoryAssignment01/CompulsoryAssignment01/Model.cs
+++ CompulsoryAssignment01/CompulsoryAssignment01/Model.cs
@@ -12,4 +12,12 @@ public class Product
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+}
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
 }
